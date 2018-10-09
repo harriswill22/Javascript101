5dc2f433d253f5f80c749697e2575d0556d84666
Will Harris <Harriswill22@yahoo.com>
2018-10-09 16:03:36 -0400

    Says Good Job for every person

---
+++ scripts/Saysgoodjob.js
@@ -0,0 +1,24 @@
+const people = [
+    'Dom',
+    'Lyn',
+    'Kirk',
+    'Autumn',
+    'Trista',
+    'Jesslyn',
+    'Kevin',
+    'John',
+    'Eli',
+    'Juan',
+    'Robert',
+    'Keyur',
+    'Jason',
+    'Che',
+    'Ben'
+  ];
+  
+let Hi_people = people.forEach(function (person) {
+let SaysGoodJob = "Good Job " + person + "!"
+console.log(SaysGoodJob)
+} 
+)
+
